726936d88d26a399ebd507acdb2a7148154ab870
Kauã <kauamoreira90@gmail.com>
2024-06-24 14:04:33 -0300

    Adicionando arquivos do projeto

---
+++ src/main/java/br/com/kgsm/PessoaContatoAPI/PessoaContatoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.kgsm.PessoaContatoAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PessoaContatoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PessoaContatoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/kgsm/PessoaContatoAPI/PessoaContatoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kgsm.PessoaContatoAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PessoaContatoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
