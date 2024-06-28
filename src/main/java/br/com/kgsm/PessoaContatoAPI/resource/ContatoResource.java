048561018f5ec8e2f1bb4a52c40f1814a11e592e
Kauã <kauamoreira90@gmail.com>
2024-06-28 17:29:20 -0300

    CRUD basico de Contato terminado

--- src/main/java/br/com/kgsm/PessoaContatoAPI/resource/ContatoResource.java
+++ src/main/java/br/com/kgsm/PessoaContatoAPI/resource/ContatoResource.java
@@ -1,9 +1,20 @@
 package br.com.kgsm.PessoaContatoAPI.resource;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.kgsm.PessoaContatoAPI.model.Contato;
 import br.com.kgsm.PessoaContatoAPI.service.ContatoService;
 
 @RestController
@@ -13,6 +24,38 @@ public class ContatoResource {
 	@Autowired
 	private ContatoService contatoService;
 	
-
+	@PostMapping
+	public ResponseEntity<Contato> save(@RequestBody Contato contato){
+		Contato newContato = contatoService.save(contato);
+		
+		if(newContato == null) return ResponseEntity.notFound().build();
+		
+		return ResponseEntity.ok(newContato);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Optional<Contato>> findById(@PathVariable Long id){
+		Optional<Contato> findContato = contatoService.findById(id);
+		
+		if(findContato == null) return ResponseEntity.notFound().build();
+		
+		return ResponseEntity.ok(findContato);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Contato> update(@PathVariable Long id, @RequestBody Contato contato){
+		Contato updContato = contatoService.update(id, contato);
+		
+		if(updContato == null) return ResponseEntity.notFound().build();
+		
+		return ResponseEntity.ok(updContato);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> delete(@PathVariable Long id){
+		contatoService.delete(id);
+		
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
 	
 }
